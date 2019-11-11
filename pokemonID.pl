02fba92aa9723a35a39cee54da6d062de9328574
fakhrurrida <52845112+fakhrurrida@users.noreply.github.com>
2019-11-11 15:10:01 +0700

    berisi identifier code bagi tokemon

---
+++ pokemonID.pl
@@ -0,0 +1,13 @@
+/* ID(T,I) : Tokemon T memiliki identifier code I */
+id(daronite,1).
+id(firara,2).
+id(burster,3).
+id(bulbaur,4).
+id(oddi,5).
+id(exegg,6).
+id(enax,7).
+id(alon,8).
+id(segirock,9).
+id(rain,10).
+id(octomon,11).
+id(dragostorm,12).
