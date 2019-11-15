1485c252bd21c38e576e28d09be70ec8b079ecec
Fakhrurrida <fakhrurridawidodo@gmail.com>
2019-11-15 16:02:15 +0700

    Update Tubes.pl

--- Tubes.pl
+++ Tubes.pl
@@ -1,19 +1,19 @@
 
 
-help:-		writeln('Available commands:'),
-			writeln('	start. --start the game!'),
-			writeln('	help. --show available commands'),
-			writeln('	quit. --quit the game'),
-			writeln('	n. s. e. w. --move'),
-			writeln('	map. --look at the map'),
-			writeln('	heal --cure Tokemon in inventory if in gym center'),
-			writeln('	status. --show your status'),
-			writeln('	save(Filename). --save your game'),
-			writeln('	load(Filename). --load previously saved game'),
-			writeln('Legends : '),
-			writeln('- X = Pagar'),
-			writeln('- P = Player'),
-			writeln('- G = Gym').
+help :-	write('Available commands:'),nl,
+write('   start. --start the game!'),nl,
+write('   help. --show available commands'),nl,
+write('   quit. --quit the game'),nl,
+write('   n. s. e. w. --move'),nl,
+write('   map. --look at the map'),nl,
+write('   heal --cure Tokemon in inventory if in gym center'),nl,
+write('   status. --show your status'),nl,
+write('   save(Filename). --save your game'),nl,
+write('   load(Filename). --load previously saved game'),nl,
+write('Legends : '),nl,
+write('- X = Pagar'),nl,
+write('- P = Player'),nl,
+write('- G = Gym'),nl.
 			
 /* Variabel Dinamik */
 :- dynamic(player_location/2).
@@ -26,38 +26,40 @@ dynamic_facts :-
 retractall(player_location(_X,_Y)),
 retractall(player_tokemon(_X)),
 retractall(tokemon_health(_X,_Y)),
-retractall(legend_count(_X)),
+retractall(legend_count(_X)).
 
-init-game:- asserta(player_location(1,1)),
+init_game:- asserta(player_location(1,1)),
 			asserta(gym_pos1(5,4)),
-			asserta(gym_pos1(14,12)),
+			asserta(gym_pos2(14,12)).
+			/*
 			random(1,12,X),
 			id(Y,X),
 			asserta(list_pokemon(Y,health(Y),normalAttack(Y),specialAttack(Y))).
-
-			
-start:- 	writeln(' ██████╗  ██████╗ ████████╗████████╗ █████╗     '),                                               
-		writeln(' ██╔════╝ ██╔═══██╗╚══██╔══╝╚══██╔══╝██╔══██╗   '),                                                
-		writeln(' ██║  ███╗██║   ██║   ██║      ██║   ███████║    '),                                               
-		writeln(' ██║   ██║██║   ██║   ██║      ██║   ██╔══██║       '),                                            
-		writeln(' ╚██████╔╝╚██████╔╝   ██║      ██║   ██║  ██║          '),                                         
-		writeln('  ╚═════╝  ╚═════╝    ╚═╝      ╚═╝   ╚═╝  ╚═╝             '),                                      
+			*/
+		
+start:- write('   ██████╗  ██████╗ ████████╗████████╗ █████╗     '),nl,                                               
+		write(' ██╔════╝ ██╔═══██╗╚══██╔══╝╚══██╔══╝██╔══██╗   '),nl,                                                
+		write(' ██║  ███╗██║   ██║   ██║      ██║   ███████║    '),nl,                                               
+		write(' ██║   ██║██║   ██║   ██║      ██║   ██╔══██║       '),nl,                                            
+		write(' ╚██████╔╝╚██████╔╝   ██║      ██║   ██║  ██║          '),nl,                                         
+		write('  ╚═════╝  ╚═════╝    ╚═╝      ╚═╝   ╚═╝  ╚═╝             '),nl,                                      
 																										   
-		writeln(' ██████╗ █████╗ ████████╗ ██████╗██╗  ██╗    ███████╗███╗   ███╗     █████╗ ██╗     ██╗     ██╗'),
-		writeln('██╔════╝██╔══██╗╚══██╔══╝██╔════╝██║  ██║    ██╔════╝████╗ ████║    ██╔══██╗██║     ██║     ██║'),
-		writeln('██║     ███████║   ██║   ██║     ███████║    █████╗  ██╔████╔██║    ███████║██║     ██║     ██║'),
-		writeln('██║     ██╔══██║   ██║   ██║     ██╔══██║    ██╔══╝  ██║╚██╔╝██║    ██╔══██║██║     ██║     ╚═╝'),
-		writeln('╚██████╗██║  ██║   ██║   ╚██████╗██║  ██║    ███████╗██║ ╚═╝ ██║    ██║  ██║███████╗███████╗██╗'),
-		writeln(' ╚═════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝    ╚══════╝╚═╝     ╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝'),
-		writeln("Gotta catch 'em all!"),
+		write(' ██████╗ █████╗ ████████╗ ██████╗██╗  ██╗    ███████╗███╗   ███╗     █████╗ ██╗     ██╗     ██╗'),nl,
+		write('██╔════╝██╔══██╗╚══██╔══╝██╔════╝██║  ██║    ██╔════╝████╗ ████║    ██╔══██╗██║     ██║     ██║'),nl,
+		write('██║     ███████║   ██║   ██║     ███████║    █████╗  ██╔████╔██║    ███████║██║     ██║     ██║'),nl,
+		write('██║     ██╔══██║   ██║   ██║     ██╔══██║    ██╔══╝  ██║╚██╔╝██║    ██╔══██║██║     ██║     ╚═╝'),nl,
+		write('╚██████╗██║  ██║   ██║   ╚██████╗██║  ██║    ███████╗██║ ╚═╝ ██║    ██║  ██║███████╗███████╗██╗'),nl,
+		write(' ╚═════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝    ╚══════╝╚═╝     ╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝'),nl,
+		write("Gotta catch 'em all!"),
 
-		writeln('Hi there! Welcome to the world of Tokemon! My name is Tome! 								'),
-		writeln('There are a lot of tokemon all around you! Catch them to get stronger! '),
-		writeln('But your final mission is to defeat or capture all those legendary tokemon, if you fail, '),
-		writeln('you will be out from this world.			'),
-		help.
-maps:- printmap(0,0),!.
-printmap(X,Y):- player_pos(Xa,Ya),
+		write('Hi there! Welcome to the world of Tokemon! My name is Tome!                              '),nl,
+		write('There are a lot of tokemon all around you! Catch them to get stronger! '),nl,
+		write('But your final mission is to defeat or capture all those legendary tokemon, if you fail, '),nl,
+		write('you will be out from this world.            '),
+		help,
+		init_game.
+map:- printmap(0,0),!.
+printmap(X,Y):- player_location(Xa,Ya),
 				X == Xa, X == Ya,
 				write('P'),
 				M is X+1,
@@ -76,7 +78,7 @@ printmap(X,Y):- gym_pos2(Xa,Ya),
 				N is Y,
 				printmap(M,N).
 printmap(X,Y):- X == 22, Y<22,
-				writeln(),
+				write(""),nl,
 				M is 0,
 				N is Y+1,
 				printmap(M,N).
